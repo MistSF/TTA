1df680b902422a4a23969ab9ab7287c72e4d3e6a
Kevin Faby <kevin@faby.fr>
2025-08-04 00:43:54 +0200

    update database

--- Back/config/database.js
+++ Back/config/database.js
@@ -1,16 +1,15 @@
 require('dotenv').config(); // Pour charger les variables d'environnement du fichier .env
 const { Sequelize } = require('sequelize');
 
-// On crée une instance de Sequelize
 const sequelize = new Sequelize(
-  process.env.DB_NAME,    // Nom de la base de données
-  process.env.DB_USER,    // Utilisateur de la base de données
-  process.env.DB_PASS,    // Mot de passe
+  process.env.MYSQLDATABASE,    
+  process.env.MYSQLUSER,    
+  process.env.MYSQLPASSWORD,    
   {
-    host: process.env.DB_HOST, // Hôte de la base de données
-    dialect: 'mysql'           // On spécifie qu'on utilise MySQL
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: 'mysql'
   }
 );
 
-// On exporte l'instance pour pouvoir l'utiliser ailleurs dans le projet
 module.exports = sequelize;
