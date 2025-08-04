3c70f085aa635e28a4c177842600ae96f40bb401
Kevin Faby <kevin@faby.fr>
2025-08-04 09:44:58 +0200

    update style App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,27 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage'; 
-import ArtisanListPage from './pages/ArtisanListPage';
-import ArtisanDetailPage from './pages/ArtisanDetailPage';
-import NotFoundPage from './pages/NotFoundPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ArtisanListPage from './pages/ArtisanListPage';
+import ArtisanDetailPage from './pages/ArtisanDetailPage';
+import SearchResultPage from './pages/SearchResultPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
-    <div className="App">
+    <div className="d-flex flex-column min-vh-100">
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/categorie/:categoryName" element={<ArtisanListPage />} />
-        <Route path="/artisan/:id" element={<ArtisanDetailPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+
+      <main className="container flex-grow-1 py-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/categorie/:categoryName" element={<ArtisanListPage />} />
+          <Route path="/search" element={<SearchResultPage />} />
+          <Route path="/artisan/:id" element={<ArtisanDetailPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
       <Footer />
     </div>
   );
